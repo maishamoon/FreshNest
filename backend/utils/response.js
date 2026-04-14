278ddaef944ceecaf9f2279f3dca20832b3d22a0
Your Name <your-email@example.com>
2026-04-14 15:02:08 +0600

    Delete previous backend folder .. Add new

--- backend/utils/response.js
+++
@@ -1,9 +0,0 @@
-function success(res, data, status = 200) {
-  return res.status(status).json({ success: true, data });
-}
-
-function error(res, msg, status = 400) {
-  return res.status(status).json({ success: false, error: msg });
-}
-
-module.exports = { success, error };
